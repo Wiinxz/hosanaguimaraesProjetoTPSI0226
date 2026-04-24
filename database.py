332f95ad963a083310dc086ff103116a69e56456
Hosana Guimarães TPSICAS0226 <hosana.guimaraes.t0133533@edu.atec.pt>
2026-04-24 22:38:16 +0100

    24/04 - Criei database e fiz testes, tudo ok

---
+++ database.py
@@ -0,0 +1,66 @@
+import sqlite3
+import os
+from datetime import datetime
+
+DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),"dental_stock.db")
+
+def documentation ():
+    """_summary_
+     Este ficheiro utiliza sqlite3 que ve incluído em python.
+     Utiliza biblioteca 'os' para construir o caminho do ficheiro
+     Utiliza a classe datetime para poder trabalhar com datas
+
+     DB_PATH utiliza o __file__ que é o caminho do própio ficheiro e o path.abspath para converter o caminh absoluto, path.dirmane pega na pasta para depois juntar com o ficheiro dental_stock.db.
+     Assim o dental_stock.db é sempre criado na mesma parta que o database.py em qualquer pc.
+
+     CATEGORIAS lista as 5 existentes no sistema, caso seja necessário criar uma nova é necessário mudar apenas aqui.
+
+     def get_connection abre ligação com a base de dados.
+
+     def criar_tabelas users e materiais. Cada material vai ter um user_id associado ao registro e data.
+     Crio já por definição o login do admin.
+
+
+
+    Returns:
+        _type_: _description_
+    """
+
+CATEGORIAS = ["Consumiveis","Dentisteria","Cirurgia","Endodontia","Prostodontia"]
+
+def get_connection():
+    return sqlite3.connect(DB_PATH)
+
+def criar_tabelas():
+
+    with get_connection() as connection:
+        connection.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+                id              INTEGER PRIMARY KEY AUTOINCREMENT,
+                email           TEXT    UNIQUE NOT NULL,
+                password        TEXT    NOT NULL,
+                data_integracao TEXT    NOT NULL,
+                role            TEXT    DEFAULT 'user'           
+            )     
+        """)
+        connection.execute("""
+           CREATE TABLE IF NOT EXISTS materiais(
+                id            INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome          TEXT    NOT NULL,
+                valor         REAL    NOT NULL,
+                categoria     TEXT    NOT NULL,
+                stock         INTEGER NOT NULL,
+                stock_minimo  INTEGER NOT NULL,
+                data_registro TEXT    NOT NULL,
+                user_id       INTEGER NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+                           )
+                    """)
+        connection.execute("""
+            INSERT OR IGNORE INTO users (email,password,data_integracao,role)
+            VALUES ('admin@clinica.pt','Admin123.','01-01-2020','admin')
+        """)
+
+        
+
+        
--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import os
+import sys
 
-print("Hello Word")
+sys.path.insert(0,os.path.dirname(__file__))
+
+import database as db
+
+#teste criação database
+
+if __name__ == "__main__":
+    db.criar_tabelas()
+    print("Hello Word")
+
+
+
+
+
+
+
