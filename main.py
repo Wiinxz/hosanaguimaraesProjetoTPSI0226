14e8f1dd9f3ef3a19d42333fbf107e3213cb4e1d
Hosana Guimarães TPSICAS0226 <hosana.guimaraes.t0133533@edu.atec.pt>
2026-05-03 13:27:13 +0100

    03/05- alterei validadores pois criei função redundante

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import validadores as validadores
 #teste criação database
 
 if __name__ == "__main__":
-    print(validadores.validar_email("admin@clinica.pt"))
-    print(validadores.validar_password("Admin123!»"))
+    print(validadores.validar_email("admin_clin@clinica.pt"))
+    print(validadores.validar_password("Admin123!"))
     print(validadores.validar_data("00-00-0000"))
     print(validadores.validar_antiguidade("24-01-2026"))
 
--- validadores.py
+++ validadores.py
@@ -30,7 +30,7 @@ def summary():
 
     """
 
-EMAIL_PATTERN = r"^[a-zA-Z0-9.-_+]+@[a-zA-Z0-9./-]+\.[a-zA-Z]{2,}$"
+EMAIL_PATTERN = r"^[a-zA-Z0-9-._+]+@[a-zA-Z0-9./-]+\.[a-zA-Z]{2,}$"
 
 PASSWORD_PATTERN = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!?@#$%^&*()_+/-=])[a-zA-Z0-9!?@#$%^&*()_+/-=]{8,}$"
 
@@ -57,10 +57,10 @@ def validar_password(password:str) -> tuple:
         return False,"A password deve conter pelo menos 1 letra minúscula"
     if not re.search(r"\d",password):
         return False,"A password deve conter pelo menos 1 número"
-    if not re.search(r"[!?@#$%^&*()_+/-=]",password):
+    if not re.search(r"^[a-zA-Z0-9!?@#$%^&*()_+/-=]{8,}$",password):
         return False,"A password deve conter pelo menos 1 caractere especial entre estes ! ? @ # $ % ^ & * ( ) _ + \ - = "
-    if not re.search(r"^[a-zA-Z0-9!?@#$%^&*()_+\-=]{8,}$",password):
-        return False, "A password contém caracteres inválidos. Use apenas letras, números e os símbolos: ! ? @ # $ % ^ & * ( ) _ + - ="
+    """if not re.search:
+        return False, "A password contém caracteres inválidos. Use apenas letras, números e os símbolos: ! ? @ # $ % ^ & * ( ) _ + - ="""
     return True,""
 
 def validar_data(data:str) -> tuple:
