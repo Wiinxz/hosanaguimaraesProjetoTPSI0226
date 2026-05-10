b5f3554309aef0fa94f9a57b487468ae49fc7ad3
Hosana Guimarães TPSICAS0226 <hosana.guimaraes.t0133533@edu.atec.pt>
2026-05-10 16:43:24 +0100

    10/05- inseri criar e autenticar user no user_repository

--- database.py
+++ database.py
@@ -61,28 +61,7 @@ def criar_tabelas():
             VALUES ('admin@clinica.pt','Admin123.','01-01-2020','admin')
         """)
 
-def criar_utilizador(email,password,data_integracao, role="user"):
 
-    try:
-        with get_connection() as conn:
-            conn.execute(
-                "INSERT INTO users (email, password, data_integracao,role) VALUES (?,?,?,?)",
-                (email,password,data_integracao,role)
-            )
-            
-            return True, "Utilizador criado com sucesso."
-    except sqlite3.IntegrityError :
-        return False, "Este email á está registrado no sistema."
-    
-def autenticar (email, password):
-
-    with get_connection() as conn:
-        
-        row = conn.execute(
-            "SELECT id, email, role FROM users WHERE email = ? AND password = ?",(email,password)
-        ).fetchone()
-    
-    return row    
     
 
 
--- users_repository.py
+++ users_repository.py
@@ -1,1 +1,33 @@
 # Apenas cria e autentica um utilizador?
+import os
+import sqlite3
+import database as data
+
+
+DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),"dental_stock.db")
+
+def get_connection():
+    return sqlite3.connect(DB_PATH)
+
+def criar_utilizador(email,password,data_integracao, role="user"):
+
+    try:
+        with get_connection() as conn:
+            conn.execute(
+                "INSERT INTO users (email, password, data_integracao,role) VALUES (?,?,?,?)",
+                (email,password,data_integracao,role)
+            )
+            
+            return True, "Utilizador criado com sucesso."
+    except sqlite3.IntegrityError :
+        return False, "Este email á está registrado no sistema."
+    
+def autenticar (email, password):
+
+    with get_connection() as conn:
+        
+        row = conn.execute(
+            "SELECT id, email, role FROM users WHERE email = ? AND password = ?",(email,password)
+        ).fetchone()
+    
+    return row    
