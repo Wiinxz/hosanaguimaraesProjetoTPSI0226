634b6d08d47be6c01bc14037f4bfc4bc91bc00a3
Hosana Guimarães TPSICAS0226 <hosana.guimaraes.t0133533@edu.atec.pt>
2026-05-12 19:48:15 +0100

    12/05 -  crio menus

--- interface.py
+++ interface.py
@@ -2,17 +2,17 @@
 import sys
 import os
 
-import authentic as authentic
+import authentic as auth
 
 def menu_principal():
-    
+
     print("\n╔══════════════════════════════════════════╗")
     print("║        MENU PRINCIPAL                    ║")
     print("╠══════════════════════════════════════════╣")
     print("║  1. Listar todos os materiais            ║")
     print("║  2. Pesquisar material                   ║")
     print("║  3. Ordenar materiais                    ║")
-    print("║  4. Estatísticas e filtros               ║")
+    print("║  4. Estatísticas                         ║")
     print("║  5. Editar material                      ║")
     print("║                                          ║")
     print("║  ── Área de Administração ──             ║")
@@ -21,3 +21,36 @@ def menu_principal():
     print("║  0. Logout                               ║")
     print("╚══════════════════════════════════════════╝")
     return input("Opção: ").strip()
+
+def menu_pesquisa():
+    print("\n╔══════════════════════════════════════════╗")
+    print("║        PESQUISA                          ║")
+    print("╠══════════════════════════════════════════╣")
+    print("║  1. Por ID                               ║")
+    print("║  2. Por Nome                             ║")
+    print("║  3. Por categoria                        ║")
+    print("║  4. VOLTAR                               ║")
+    print("╚══════════════════════════════════════════╝")
+    return input("Opção: ").strip()
+
+def menu_ordenacao():
+    print("\n╔══════════════════════════════════════════╗")
+    print("║        ORDENAÇÃO                         ║")
+    print("╠══════════════════════════════════════════╣")
+    print("║  1. bUBBLE SORT                          ║")
+    print("║  2. SELECTION SORT                       ║")
+    print("║  3. SAIR                                 ║")
+    print("╚══════════════════════════════════════════╝")
+    return input("Opção: ").strip()
+
+def menu_estatisticas():
+    print("\n╔══════════════════════════════════════════╗")
+    print("║        ESTATÍSTICAS                      ║")
+    print("╠══════════════════════════════════════════╣")
+    print("║  1. RESUMO ESTATISTICO?                  ║")
+    print("║  2. MATERIAIS COM STOCK ABAIXO DO MÍNIMO ║")
+    print("║  3. SAIR                                 ║")
+    print("╚══════════════════════════════════════════╝")
+    return input("Opção: ").strip()
+
+
